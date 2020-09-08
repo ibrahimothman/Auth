d8b1a45162fd4a65ecada4e1bce78f9b31eb0038
ibrahimothman <ibrahimothmanre@gmail.com>
2020-09-08 19:36:58 +0200

    create the express server

---
+++ server/server.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const morgan = require('morgan')
+
+dotenv.config()
+
+const app = express()
+app.use(morgan('dev'))
+
+app.get('/', (req, res) => {
+    res.json({
+        message: "welcome to auth"
+    })
+})
+
+// some global middleware
+
+function notFount(req, res, next) {
+    res.status(404);
+    const error = new Error(`Not found - ${req.originalUrl}`);
+    next(error);
+}
+
+function errorHandler(req, res, next) {
+    console.log('error handler');
+    res.status(res,status || 500);
+    res.json({
+        message: err.message,
+        stack: err.stack
+    });
+}
+
+app.use(notFount)
+app.use(errorHandler)
+
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`Server is running on ${port} port`))
